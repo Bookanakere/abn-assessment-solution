25c87ae04a292dad045f5996058d9d8410a21cc1
Bookanakere <rash.pb@gmail.com>
2021-11-08 15:55:15 +0530

    minor update

--- src/test/java/starter/steps/ValidateHomePageSteps.java
+++ src/test/java/starter/steps/ValidateHomePageSteps.java
@@ -8,20 +8,20 @@ import static org.junit.Assert.assertTrue;
 
 public class ValidateHomePageSteps {
 
-    private HomePage homePage1;
+    private HomePage homePage;
 
     @Step("Verify test on home page")
     public void verifyTextOnHomePage(String textOnHomePage){
-        assertEquals(textOnHomePage, homePage1.getTextOnHomePage());
+        assertEquals(textOnHomePage, homePage.getTextOnHomePage());
     }
 
     @Step("Validate title on the homepage screen")
     public void validateHomePageTitle(String homePageTitle){
-        assertEquals(homePageTitle, homePage1.getHomePageTitle());
+        assertEquals(homePageTitle, homePage.getHomePageTitle());
     }
 
     @Step("Validate mail icon exists on the homepage screen")
     public void validateMailIconExistsOnHomePage(){
-        assertTrue(homePage1.checkMailIconExists());
+        assertTrue(homePage.checkMailIconExists());
     }
 }
